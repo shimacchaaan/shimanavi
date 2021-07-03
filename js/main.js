913630298cb93ed6989735879c38899ef20736ec
hisanori <wwshimabara@gmail.com>
2021-07-04 08:54:01 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 $(function() {
-  $('#btn-june').click(function(){
-    $('.june').fadeIn();
-  });
-
   // アコーディオン化する
   $('#btn-june').click(function() {
     var $june = $(this).find('.june');
     if($june.hasClass('open')) {
       $june.removeClass('open');
       // slideUpメソッドを用いて、$answerを隠してください
-      $june.slideUp();
+      $june.fadeIn();
 
       // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
       $(this).find('span').text('+');
-      
+
     } else {
       $june.addClass('open');
       // slideDownメソッドを用いて、$answerを表示してください
-      $june.slideDown();
+      $june.fadeOut();
 
       // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
       $(this).find('span').text('-');
