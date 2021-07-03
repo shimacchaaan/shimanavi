1c9605cbfa018870b02f674eb139e99f121b1054
hisanori <wwshimabara@gmail.com>
2021-07-04 08:46:25 +0900

    acodion

--- js/main.js
+++ js/main.js
@@ -2,4 +2,27 @@ $(function() {
   $('#btn-june').click(function(){
     $('.june').fadeIn();
   });
+
+  // アコーディオン化する
+  $('#btn-june').click(function() {
+    var $june = $(this).find('.june');
+    if($june.hasClass('open')) {
+      $june.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $june.slideUp();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('+');
+      
+    } else {
+      $june.addClass('open');
+      // slideDownメソッドを用いて、$answerを表示してください
+      $june.slideDown();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('-');
+
+    }
+  });
+
 });
